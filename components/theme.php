de083713bf8ac63b9e83683db33b18e8dd0489b3
Tony Barry <TonyMPBarry@gmail.com>
2018-06-01 13:53:31 -0700

    Add page type class to <body> classlist

--- components/theme.php
+++ components/theme.php
@@ -1,5 +1,6 @@
 <?php
 
+use TBPixel\PageType\Page;
 use TBPixel\PageType\Bundle;
 
 
@@ -25,7 +26,7 @@ function pagetype_theme() : array
 function theme_pagetype_admin_types(array $variables) : string
 {
     $type = $variables['type'];
-    
+
     if (!$type instanceof Bundle) return '';
 
     $output = check_plain($type->name);
@@ -34,4 +35,24 @@ function theme_pagetype_admin_types(array $variables) : string
 
 
     return $output;
+}
+
+
+/**
+ * hook_preprocess_html()
+ */
+function pagetype_preprocess_html(array &$vars) : void
+{
+    if (!(strpos($path = current_path(), 'pages/') === 0)) return;
+
+    $id   = explode('/', $path);
+    $id   = end($id);
+
+    if (!is_numeric($id) || is_null($page = Page::findOne($id))) return;
+
+    $class = drupal_clean_css_identifier(
+        'page--' . str_replace('_', '-', $page->type)
+    );
+
+    array_unshift($vars['classes_array'], $class);
 }
